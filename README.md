a848871435b70381824fb0639c40ab16a42f04f9
salo1893 <salome.mueller@alansa.ch>
2021-09-05 10:04:08 +0200

    Updated readMe, better time measurement and output

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # cyk-algorithm
 Comparison of different implementations of the CYK-algorithm in Java, as part of an Lecture about efficient algorithms
+
+##Usage
+This implementation of CYK takes an input grammar in CNF and computes for a set of test strings, whether the strings are in the language of this grammar.
+The output is a table containing for each string its length, the computation time in seconds, the counter and the computed value.
+
+The program has three algorithms from which the user can choose, a naive approach ('naive'), the buttom up implementation ('bu') and top-down ('td').
+The chosen algorithm can be passed as first program argument, the default is top-down.
+For the naive approach, which uses a recursive implementation, counter is the number of calls of the recursive function it took to compute the result.
+For bottom-up and top-down it counts how often the inner-most loop is executed.
+
+The second ürogram argument is the text file containing the rules of the grammar.
+If none is passed, then input.txt is used, which contains a simple exemplary grammar.
--- src/CYK.java
+++ src/CYK.java
@@ -1,3 +1,5 @@
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -13,7 +15,7 @@ public class CYK {
         String[] test = {"()", "()()", " ())", "(())", ")("};
 
         // construct grammar from the input file
-        Grammar grammar = null;
+        Grammar grammar;
         try {
             grammar = new Grammar(input);
         } catch (Exception e) {
@@ -26,29 +28,29 @@ public class CYK {
         // print info and head of table
         String alg = algorithm.equals("naive") ? "naive" : algorithm.equals("bd") ? "bottom-down" : "top-down";
         System.out.print("\nStarting computation with the " + alg + " algorithm for " + test.length + " input strings:\n\n");
-        System.out.printf("%-7s| %-7.4s | %-7s | %-7s \n", "Length", "Time", "Counter", "Truth-Value");
-        System.out.print("-----------------------------------------\n");
+        System.out.printf("%-7s| %-15s | %-15s | %-7s \n", "Length", "Time in seconds", "Counter", "Truth-Value");
+        System.out.print("---------------------------------------------------------\n");
 
         Parser parser = new Parser(grammar);
         boolean in_language;
-        float start;
-        float end;
+        Instant start;
+        int end;
 
         // parse and meassure all strings
         for (String s : test) {
 
             // start time measurement
-            start = System.currentTimeMillis();
+            start = Instant.now();
 
             // parse the current string
             in_language = parser.parse(s, algorithm);
 
-            // stop time measurement
-            end = (System.currentTimeMillis() - start) / 1000;
+            // compute duration of execution
+            end = Duration.between(start ,Instant.now()).toSecondsPart();
 
             // TODO: maybe run parser multiple times and take average (in this case the input string should be parsed beforehand
             // print result
-            System.out.printf("%-7d|  %-7.4f|  %-7d|  %-7b\n", s.length(), end, Parser.counter, in_language);
+            System.out.printf("%-7d|  %-15d|  %-15d|  %-7b\n", s.length(), end, Parser.counter, in_language);
         }
     }
 
