64dd30005444318479f372fe0a1f1942b94a3a74
salo1893 <salome.mueller@alansa.ch>
2021-09-06 19:25:10 +0200

    added top down, generated big testset

--- src/Parser.java
+++ src/Parser.java
@@ -6,17 +6,21 @@ public class Parser {
     static int counter;
     Grammar grammar;
     char[] input;
+    boolean[][][] tab; // used in top-down (and buttom-up)
+    boolean[][][] tab_set; // used in top-down (and buttom-up)
 
     /**
      * Constructor for the Parser.
+     *
      * @param grammar grammar this parser shall use
      */
-    Parser (Grammar grammar) {
+    Parser(Grammar grammar) {
         this.grammar = grammar;
     }
 
     /**
      * Parse method of the grammar. Parses the input string with the chosen parser, if it contains only terminal characters.
+     *
      * @param s input string
      * @return true if the input is a word of this grammar, false otherwise
      */
@@ -44,14 +48,14 @@ public class Parser {
         if (i == j - 1) {
             // i = j-1 => A -> s[i] must be a terminal rule
             for (Character c : grammar.getTerminalRules(non_terminal)) {
-                if ( c.equals(input[i])) return true;
+                if (c.equals(input[i])) return true;
             }
         } else {
             // A -> BC => B must form to any left part (i-k), C the corresponding right part (k-j) 
             // iterate through all rules of nonterminal A, for every rule try every splitting point k.
-            for (Integer[] rule: grammar.getRules(non_terminal)) {
+            for (Integer[] rule : grammar.getRules(non_terminal)) {
                 for (int k = i + 1; k < j; k++) {
-                    if (parseNaive(rule[0], i, k) && parseNaive(rule[1], k , j)) return true;
+                    if (parseNaive(rule[0], i, k) && parseNaive(rule[1], k, j)) return true;
                 }
             }
         }
@@ -60,11 +64,13 @@ public class Parser {
     }
 
     private boolean parseBU(char[] s) {
-        boolean[][][] tab = new boolean[grammar.nr_non_terminals()][s.length][s.length];
+        // allocate table, java initialises bool arrays with false for every cell
         // array: literal, start position, length of substr - 1
+        tab = new boolean[grammar.nr_non_terminals()][s.length][s.length];
+
         // Fill first row of table: enter true for all non-terminal A, row i column 1 if A->s[i] is a terminal rule
         for (int i = 0; i < s.length; i++) {
-            for (Integer a: grammar.getTerminalRules(s[i])) {
+            for (Integer a : grammar.getTerminalRules(s[i])) {
                 tab[a][i][0] = true;
             }
         }
@@ -72,18 +78,18 @@ public class Parser {
         Integer[][][] rules = grammar.getRules();
 
         // j = length of substring (2 - n)
-       for (int j = 1; j < s.length; j++) {
+        for (int j = 1; j < s.length; j++) {
             // i = start of substing
             for (int i = 0; i < s.length - j; i++) {
-                // k = splitting point
-                for (int k = 0; k < j; k++) {
-                    // a = non-terminal
-                    for (int a = 0; a < rules.length; a++) {
-                        // rule = every non-terminal rule of a
-                        for (Integer[] rule : rules[a]) {
+                // a = non-terminal
+                for (int a = 0; a < rules.length; a++) {
+                    // rule = every non-terminal rule of a
+                    for (Integer[] rule : rules[a]) {
+                        // k = splitting point
+                        for (int k = 0; k < j; k++) {
                             // check whether first letter can produce substring i till k and second i+k+1 till j-k-1
-                            boolean both_sides = tab[rule[0]][i][k] && tab[rule[1]][i+k+1][j-k-1];
-                            tab[a][i][j] = tab[a][i][j] || both_sides;
+                            tab[a][i][j] = tab[a][i][j] ||
+                                    (tab[rule[0]][i][k] && tab[rule[1]][i + k + 1][j - k - 1]);
                             counter++;
                         }
                     }
@@ -96,9 +102,49 @@ public class Parser {
     }
 
     private boolean parseTD(char[] s) {
-        // TODO
-        // increase counter per most inner loop
-        return false;
+        tab = new boolean[grammar.nr_non_terminals()][s.length][s.length];
+        // java initialises bool arrays with false, tab_set is to check whether cell has been allocated before
+        tab_set = new boolean[grammar.nr_non_terminals()][s.length][s.length];
+
+        return parseTD(0, 0, s.length - 1);
+    }
+
+    /**
+     * PArse the input string with a top-down approach
+     *
+     * @param a a non-terminal
+     * @param i the start point of the current substring
+     * @param j length of the current substring
+     * @return whether a can produce the substring
+     */
+    private boolean parseTD(int a, int i, int j) {
+        if (!tab_set[a][i][j]) {
+            // cell has not yet been computed, but will now -> set tab_set to true
+            tab_set[a][i][j] = true;
+
+            if (j == 0) {
+                // j = 0 => A -> s[i] must be a terminal rule
+                for (Character c : grammar.getTerminalRules(a)) {
+                    if (c.equals(input[i])) {
+                        tab[a][i][j] = true;
+                        break;
+                    }
+                }
+            } else {
+                // A -> BC => B must form to any left part (i-k), C the corresponding right part (k-j)
+                // iterate through all rules of non-terminal A, for every rule try every splitting point k.
+                for (Integer[] rule : grammar.getRules(a)) {
+                    for (int k = 0; k < j; k++) {
+                        counter++;
+                        // check whether first letter can produce substring i till k and second i+k+1 till j-k-1
+                        tab[a][i][j] = tab[a][i][j] ||
+                                (parseTD(rule[0], i, k) && parseTD(rule[1], i + k + 1, j - k - 1));
+                    }
+                }
+            }
+        }
+
+        return tab[a][i][j];
     }
 
 }
